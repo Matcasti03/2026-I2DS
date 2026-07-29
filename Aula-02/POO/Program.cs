c0e0e1283c58a2b1448a020bc4a6c2f76d33f315
Matcast-ox <matcastilho03@gmail.com>
2026-07-29 16:40:01 -0300

    Aula 02- Classe e objetoo

--- Aula-02/POO/Program.cs
+++ Aula-02/POO/Program.cs
@@ -1,1 +1,58 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //comentario de linha
+        /*comentario de blocooooooo
+
+
+        varias linhas 
+        */
+
+        //imprimir uma linha de texto na tela
+        Console.WriteLine("Programacao Orienta a Objetos\n");
+
+        Console.WriteLine("Abstracao - 1° pilar da POO");
+        
+        //Criar uma classe em C#
+
+//A classe deve ser declarada fora do bloco programa
+
+//criar um objeto apartir de uma classe
+    Pet pet1 = new Pet();
+pet1.nome = "Totó";
+pet1.tipo = "Cachorro";
+pet1.idade = 3;
+
+
+pet1.MostrarNome();
+
+Pet pet2 = new Pet();
+pet2.nome = "Mingau";
+pet2.tipo = "gato";
+pet2.idade = 8;
+
+
+pet2.MostrarNome();
+
+    }
+
+
+public class Pet //Nome da classe sempre se inicia acom letras maiuscula
+
+    {
+        //declaracao dos atributos
+public string nome;
+public string tipo;
+public int idade;
+
+        //declaracao dos metodos
+        public void MostrarNome()
+        {
+            Console.WriteLine("Meu nome é " + this.nome);
+        }
+    }
+    
+}
--- Aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
+++ Aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf6617db7fd45fa8f6ec881f72cdbe3c7233e517")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a906c8b5d9f2a10ec2ee9b8735f6dadc1ae79bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("POO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
