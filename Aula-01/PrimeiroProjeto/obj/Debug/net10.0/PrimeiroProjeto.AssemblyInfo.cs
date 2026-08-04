a1e58191583400084c4875ee8017ccda802b52ad
Matcast-ox <matcastilho03@gmail.com>
2026-08-04 08:29:31 -0300

    classe e objeto

--- Aula-01/PrimeiroProjeto/obj/Debug/net10.0/PrimeiroProjeto.AssemblyInfo.cs
+++ Aula-01/PrimeiroProjeto/obj/Debug/net10.0/PrimeiroProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+881a14bebcfa9e3f6600810211c3006f24b76672")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0e0e1283c58a2b1448a020bc4a6c2f76d33f315")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
+++ Aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a906c8b5d9f2a10ec2ee9b8735f6dadc1ae79bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0e0e1283c58a2b1448a020bc4a6c2f76d33f315")]
 [assembly: System.Reflection.AssemblyProductAttribute("POO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
