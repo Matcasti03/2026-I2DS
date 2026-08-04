983694b642558cc598cd57f37953e5f793ad22cb
Matcast-ox <matcastilho03@gmail.com>
2026-08-04 16:20:33 -0300

    iniciando os exercicos de C#

---
+++ Aula-03/Exercicios/Exercicio-01/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+   
+   
+    /*Criar um programa capaz de converter temperaturas entre diferentes unidades.
+
+O exercício tem como objetivo trabalhar:
+
+- Variáveis;
+- Entrada de dados;
+- Operações matemáticas;
+- Métodos. */
+
+//Precisamos configurar uma variavel para CELCIUS
+
+double celcius = 27;
+double Fahrenheit = 0;
+
+//fazemos a conversao entre as unidades!
+
+Console.WriteLine("A temperatura convertida de Celcius para Fahrenheit é: " + celcius * 9 );
+
+
+    }
+}
