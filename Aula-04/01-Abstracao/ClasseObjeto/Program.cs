6a32aab3473ddee7ba1af7b7ac28ae26b973560a
Matcast-ox <matcastilho03@gmail.com>
2026-08-05 15:56:40 -0300

    Aula 4 - pilares da orientacao em objeto \

---
+++ Aula-04/01-Abstracao/ClasseObjeto/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+      //Classe -> É a abstracao de um objeyo do mundo real para o mundo computacional,
+      //objeto -> É a instancia de uma classe.
+
+//Instanciar um projeto do tipo aluno
+Aluno aluno01 = new Aluno();
+Aluno aluno02 = new Aluno();
+
+aluno01.Nome = "Matheus Castilho";
+aluno01.RM = 2444;
+aluno01.DataNascimento = new DateOnly(2010,01,15);
+
+aluno02.Nome = "João dos Santos";
+aluno02.RM = 1234;
+aluno02.DataNascimento = new DateOnly(1006,12,07);
+
+//Executar metodos
+aluno01.ApresentarSE();
+aluno02.ApresentarSE();
+
+    }
+
+    //Delcaracao de uma classe
+        public class Aluno //Declaracao de uma classe
+    {
+        //Atributos -> Caracteristicas
+        public string Nome { get; set; }
+        public int RM {get; set;}
+        public DateOnly DataNascimento {get; set;}
+
+        //Metodos -> Acoes ou funcionalidades
+        public void ApresentarSE()
+        {
+            Console.WriteLine($"Meu nome é {Nome}, Meu RM é {RM} e nasci no dia {DataNascimento}");
+        }
+    } 
+}
---
+++ Aula-04/02-Heranca/Heranca/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Dynamic;
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Heranca: uma sub classe ou conhecida tambem como classe filha, recebe os atributos e metodos da super classe
+        animal animal1 = new animal();
+        Cachorro dog1 = new Cachorro();
+
+        animal1.Nome = "Ferdinando";
+        animal1.Idade = 2;
+        Console.WriteLine($"Nome do animal: {animal1.Nome}  -  Idade {animal1.Idade}");
+
+        dog1.Nome = "Snoop";
+        dog1.Idade = 5;
+        dog1.Raca = "Vira-Lata";
+        dog1.Latir();
+
+        Console.WriteLine($"Nome:  {dog1.Nome}  |  Idade:  {dog1.Idade}  |  Raça:  {dog1.Raca}");
+    }
+    //Super classe animal (MÃE)
+
+public class animal 
+{
+public string Nome {get; set;}
+public int Idade {get; set;}
+}
+
+//Sub Claasse (Filha)
+
+public class Cachorro : animal
+    {
+        public string Raca {get; set;}
+        public void Latir()
+        {
+            Console.WriteLine("Au Au");
+
+        }
+    }
+}
