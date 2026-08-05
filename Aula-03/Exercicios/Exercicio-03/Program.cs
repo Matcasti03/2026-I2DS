e55fb21c21d9484648f15e1a586216d40ffae555
Matcast-ox <matcastilho03@gmail.com>
2026-08-05 09:38:23 -0300

    acabam os o exercicio 03

--- Aula-03/Exercicios/Exercicio-03/Program.cs
+++ Aula-03/Exercicios/Exercicio-03/Program.cs
@@ -2,6 +2,37 @@
 {
     private static void Main(string[] args)
     {
-    
+        double nota1;
+        double nota2;
+        double nota3;
+        double media;
+        string nome;
+
+        Console.WriteLine("Digite seu nome: ");
+        nome = Console.ReadLine();
+        Console.WriteLine("Acesso Permitido! ");
+        
+        Console.WriteLine("Agora, digite sua Nota 1: ");
+        nota1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Agora, digite sua Nota 2: ");
+        nota2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Agora, digite sua Nota 3: ");
+        nota3 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Calculando... ");
+
+        media = (nota1 + nota2 + nota3) / 3;
+
+
+        if (media >= 7)
+        {
+            Console.WriteLine(nome + ", você foi Aprovado, Média: " + media);
+        }
+        else
+        {
+            Console.WriteLine(nome + ", você foi Reprovado, Média: " + media);
+        }
     }
 }
--- Aula-03/Exercicios/Exercicio-03/obj/Debug/net10.0/Exercicio-03.AssemblyInfo.cs
+++ Aula-03/Exercicios/Exercicio-03/obj/Debug/net10.0/Exercicio-03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio-03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e120ec25d0de7b35cdd7e174a4408e1aea06fd29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc020bb1a21208ce7e4e4d57fab4e95fcd807515")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio-03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio-03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
